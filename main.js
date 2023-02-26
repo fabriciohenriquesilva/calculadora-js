6a631a34974a741de28c2a36712845a79dc23d94
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2023-02-26 18:01:02 -0300

    refatorada a função de obter os números e adicionado as funcionalidades aos botões C, CE e igual

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 let inputExpressao = document.querySelector('#expressao');
 let inputResultado = document.querySelector('#resultado');
 
-inputExpressao.addEventListener('blur', () => {
-    // let expressao = inputExpressao.value;
-    let expressao = '-20 + 2 * 6 / ( 2 - 7)';
+let btnLimparHistorico = document.querySelector('#limpar');
+btnLimparHistorico.addEventListener('click', () => {
+    inputResultado.value = '';
+});
+
+let btnCalcular = document.querySelector('#calcular');
+btnCalcular.addEventListener('click', calcular);
+
+let btnLimparEntrada = document.querySelector('#cancelar');
+btnLimparEntrada.addEventListener('click', () => {
+    inputExpressao.value = '';
+})
+
+function calcular() {
+    let expressao = inputExpressao.value;
+    // let expressao = '-20 + 2 * 6 / ( 2 - 7)';
     // let expressao = '5 + 4 - 3 + 6 - 3 / (1 * 5)';
     console.log(expressao);
 
-    let operandos = extraiOperandos(expressao);
+    let operandos = extraiNumeros(expressao);
     let operadores = extraiOperadores(expressao);
 
-    console.log("L13 -> ", operandos);
-    console.log("L14 -> ", operadores);
+    console.log("L26 -> ", operandos);
+    console.log("L27 -> ", operadores);
 
     while (operadores.length > 0) {
         let indice = procuraPrimeiraOperacao(operadores);
@@ -24,11 +37,11 @@ inputExpressao.addEventListener('blur', () => {
             let op = operadores.splice(indice + 1, 1);
             operadores.splice(operadores.indexOf(')'), 1);
             c = aplicar(a, b, op);
-            console.table([a, op, b, c]);
+            // console.table([a, op, b, c]);
         }
         else {
             c = aplicar(a, b, op);
-            console.table([a, op, b, c]);
+            // console.table([a, op, b, c]);
         }
 
         operandos[indice] = c;
@@ -41,6 +54,12 @@ inputExpressao.addEventListener('blur', () => {
     }
 
     console.log(operandos);
+    let resultado = operandos[0];
+    inputResultado.value += `${expressao} = ${resultado} \n`;
+}
+
+inputExpressao.addEventListener('blur', () => {
+    
 
 });
 
@@ -82,19 +101,6 @@ function procuraPrimeiraOperacao(operadores) {
     return indice;
 }
 
-function extraiOperandos(expressao) {
-    let arr = expressao.split(/[+*\/()]|[\s]/)
-        .filter(e => e != '');
-
-    while(arr.findIndex(e => e == '-') > 0){
-        let i = arr.findIndex(e => e == '-');
-        arr[i+1] *= -1;
-        arr.splice(i, 1);
-    }
-    return arr.map(e => parseFloat(e));
-
-}
-
 function extraiOperadores(expressao) {
     let arr = expressao.split(/\s?\d+\s?|\s/)
         .filter(e => e != '');
@@ -102,6 +108,9 @@ function extraiOperadores(expressao) {
     if(arr[0] == '-'){
         arr.splice(0, 1);
     }
+    
+    arr = verificaErroDeDivisaoPorParenteses(arr);
+
     for(let i = 0; i < arr.length; i++) {
         if(arr[i] == '-') {
             arr[i] = '+';
@@ -109,4 +118,60 @@ function extraiOperadores(expressao) {
     }   
     
     return arr;
+}
+
+function extraiNumeros(expressao) {
+    let arr = expressao.replaceAll(',', '.').split('').filter(e => e != ' ');
+
+    let numeroString = '';
+    let numeros = [];
+
+    console.log(arr);
+
+    for(let i = 0; i < arr.length; i++){
+        console.log("arr[i]", arr[i]);
+        if(/\d/.test(arr[i]) || arr[i] == '-' || arr[i] == '.'){
+            numeroString += arr[i];
+
+            if(i+1 >= arr.length) {
+                numeros.push(numeroString);
+            }
+
+            for(let j = i+1; j < arr.length; j++) {
+                console.log("v[j]", arr[j])
+                if(/\d/.test(arr[j]) || arr[j] == '.') {
+                    numeroString += arr[j];
+                    console.log("L141", i, j, arr[j], numeroString);
+                    i = j;
+                }
+                if(arr[j] == '-'){
+                    numeros.push(numeroString);
+                    numeroString = '';
+                    break;
+                }
+                if(/[+*\/()]/.test(arr[j]) || j == arr.length-1) {
+                    i = j;
+                    if(arr[j] != '('){
+                        numeros.push(numeroString);
+                        numeroString = '';
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    return numeros.map(e => parseFloat(e));
+}
+
+function verificaErroDeDivisaoPorParenteses(arr) {
+    let i = arr.findIndex(e => e == '/(');
+    let copy = arr;
+    if(i > 0) {
+        copy = arr.slice(0, i);
+        copy.push('/', '(');
+        copy.push(...arr.slice(i+1));
+    }
+
+    return copy;
+
 }
